967d555d56bba5c31eb45f8251ed694f3864611b
NZY\NZY <mandreicosmin@yahoo.com>
2019-06-13 18:48:03 +0300

    assync db created successfully

--- src/indexDB.js
+++ src/indexDB.js
@@ -5,29 +5,52 @@ const customerData = [
     { ssn: "555-55-5555", name: "Donna", age: 32, email: "donna@home.org" }
   ];
 
-const request = window.indexedDB.open("MyDatabasePhotoApp", 1);
-
-
-// only in this place we can alter the structure of the database (create, delete objects stores);
-request.onupgradeneeded = e => {
-    const db = e.target.result;
-    const objectStore = db.createObjectStore('accounts');
+const dbDetails = {
+    name: 'MyDatabasePhotoApp',
+    version: 1
 }
 
-request.onsuccess = e => {
-    console.log('succes');
-    const db = request.result;
-    var obStore = db.transaction('accounts', "readwrite").objectStore("accounts");
-    // const req = obStore.get('myData');
-    
-    obStore.put('Ana are mere', 'my-data')
-    
-    customerData.forEach((customer, index) => {
-        obStore.put(customer.name, index ) 
-    })
+function createDB() {
+    return new Promise((resolve, reject) => {
+        // creating db
+        const request = window.indexedDB.open(dbDetails.name, dbDetails.version);
+        
+        // only in this place we can alter the structure of the database (create, delete objects stores);
+        request.onupgradeneeded = e => {
+            const db = e.target.result;
+            const objectStore = db.createObjectStore('accounts');
+        }
+
+        request.onsuccess = e => {
+            // console.log('succes');
+            
+            const db = request.result;
+            const obStore = db.transaction('accounts', 'readwrite').objectStore('accounts');
+
+            // put data into db
+            obStore.put('Ana are mere', '#1' );
+            resolve('Successfully resolved my DB');
+        }
+
+        request.onerror = e => {
+            reject(request.e);
+        }
+    });
 }
 
-request.onerror = e => {
-    console.log('Error', e);
+// check if the DB is successfully created or not :D:D 
+async function testDb() {
+    try {
+        const checkDb = await createDB();
+        console.log(checkDb);
+    } catch(err) {
+        console.log(`Error, ${err}`)
+    }
 }
+
+testDb();
+
+
+
+
 
